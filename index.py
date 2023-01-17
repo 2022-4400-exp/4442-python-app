7c65643738306fff9e02afe88ad790a4e51fdcf4
m19153 <m19153@g.metro-cit.ac.jp>
2023-01-17 13:27:18 +0900

    change: 掲示板

--- index.py
+++ index.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+import os
+
 app = Flask(__name__)
  
 @app.route('/', methods=["GET"])
 def index():
-    hello = "Hello world"
-    return hello
+    # 掲示板データファイルを開いてみる
+    try:
+        # テキストファイルを開く
+        with open('/var/python-app/data.txt') as f:
+            # 読み込み
+            lines = f.read()
+    # ファイルがなかった場合、変数を空にする
+    except IOError:
+        lines = ""
+    # ファイルから読み込んだデータをHTMLに渡す
+    return render_template('index.html', data = lines.split("\n"))
 
 if __name__ == '__main__':
+    # フォルダの作成
+    os.makedirs("/var/python-app", exist_ok=True)
+    # Flask起動
     app.run(host='0.0.0.0', port=5000, debug=True)
